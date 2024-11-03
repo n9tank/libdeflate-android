90376aaa40deda905228647ae94e744e0e256735
n9tank <n9tank@163.com>
2024-11-03 22:12:31 +0800

    删了一些没必要的api

--- app/src/main/c/checksums.c
+++ app/src/main/c/checksums.c
@@ -18,63 +18,61 @@
 
 LIBDEFLATEJAVA_PUBLIC JNIEXPORT jint JNICALL
 Java_me_steinborn_libdeflate_LibdeflateCRC32_crc32Heap(JNIEnv *env,
-                                                       jclass klass,
-                                                       jlong crc32,
-                                                       jbyteArray array,
-                                                       jint off, jint len) {
-  jbyte *arrayBytes = (*env)->GetPrimitiveArrayCritical(env, array, 0);
-  if (arrayBytes) {
-    crc32 = (jint)libdeflate_crc32((uint32_t)crc32, (void *)(arrayBytes + off),
-                                   len);
-    (*env)->ReleasePrimitiveArrayCritical(env, array, arrayBytes, JNI_ABORT);
-  }else {
-  throwException(env, "java/lang/OutOfMemoryError",NULL);
-  }
-  return crc32;
+													   jclass klass,
+													   jlong crc32,
+													   jbyteArray array,
+													   jint off, jint len)
+{
+	jbyte *arrayBytes = (*env)->GetPrimitiveArrayCritical(env, array, 0);
+	if (arrayBytes)
+	{
+		crc32 = (jint)libdeflate_crc32((uint32_t)crc32, (void *)(arrayBytes + off),
+									   len);
+		(*env)->ReleasePrimitiveArrayCritical(env, array, arrayBytes, JNI_ABORT);
+	}
+	else
+	{
+		throwException(env, "java/lang/OutOfMemoryError", NULL);
+	}
+	return crc32;
 }
 
 LIBDEFLATEJAVA_PUBLIC JNIEXPORT jint JNICALL
 Java_me_steinborn_libdeflate_LibdeflateCRC32_crc32Direct(
-    JNIEnv *env, jclass klass, jlong crc32, jobject buf, jint off, jint len) {
-  jbyte *bufBytes = (*env)->GetDirectBufferAddress(env, buf);
+	JNIEnv *env, jclass klass, jlong crc32, jobject buf, jint off, jint len)
+{
+	jbyte *bufBytes = (*env)->GetDirectBufferAddress(env, buf);
 
-  if (bufBytes == NULL) {
-    throwException(env, "java/lang/IllegalArgumentException",
-                   "unable to obtain direct access to buffer");
-    return -1;
-  }
-
-  return (jint)libdeflate_crc32((uint32_t)crc32, (void *)(bufBytes + off), len);
+	return (jint)libdeflate_crc32((uint32_t)crc32, (void *)(bufBytes + off), len);
 }
 
 LIBDEFLATEJAVA_PUBLIC JNIEXPORT jint JNICALL
 Java_me_steinborn_libdeflate_LibdeflateAdler32_adler32Heap(JNIEnv *env,
-                                                           jclass klass,
-                                                           jlong adler32,
-                                                           jbyteArray array,
-                                                           jint off, jint len) {
-  jbyte *arrayBytes = (*env)->GetPrimitiveArrayCritical(env, array, 0);
-  if (arrayBytes) {
-    adler32 = (jint)libdeflate_adler32((uint32_t)adler32,
-                                       (void *)(arrayBytes + off), len);
-    (*env)->ReleasePrimitiveArrayCritical(env, array, arrayBytes, JNI_ABORT);
-  } else {
-  throwException(env, "java/lang/OutOfMemoryError",NULL);
-  }
-  return adler32;
+														   jclass klass,
+														   jlong adler32,
+														   jbyteArray array,
+														   jint off, jint len)
+{
+	jbyte *arrayBytes = (*env)->GetPrimitiveArrayCritical(env, array, 0);
+	if (arrayBytes)
+	{
+		adler32 = (jint)libdeflate_adler32((uint32_t)adler32,
+										   (void *)(arrayBytes + off), len);
+		(*env)->ReleasePrimitiveArrayCritical(env, array, arrayBytes, JNI_ABORT);
+	}
+	else
+	{
+		throwException(env, "java/lang/OutOfMemoryError", NULL);
+	}
+	return adler32;
 }
 
 LIBDEFLATEJAVA_PUBLIC JNIEXPORT jint JNICALL
 Java_me_steinborn_libdeflate_LibdeflateAdler32_adler32Direct(
-    JNIEnv *env, jclass klass, jlong adler32, jobject buf, jint off, jint len) {
-  jbyte *bufBytes = (*env)->GetDirectBufferAddress(env, buf);
+	JNIEnv *env, jclass klass, jlong adler32, jobject buf, jint off, jint len)
+{
+	jbyte *bufBytes = (*env)->GetDirectBufferAddress(env, buf);
 
-  if (bufBytes == NULL) {
-    throwException(env, "java/lang/IllegalArgumentException",
-                   "unable to obtain direct access to buffer");
-    return -1;
-  }
-
-  return (jint)libdeflate_adler32((uint32_t)adler32, (void *)(bufBytes + off),
-                                  len);
+	return (jint)libdeflate_adler32((uint32_t)adler32, (void *)(bufBytes + off),
+									len);
 }
